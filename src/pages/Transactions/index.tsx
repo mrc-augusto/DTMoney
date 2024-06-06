d33565099a69326b96ceafb081ac9b374148e760
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2024-06-06 17:27:17 -0300

    creation of hooks, contexts and other

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
-interface Transaction{
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
 export function Transactions(){
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions(){
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-   setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
-
+  const {transactions} = useContext(TransactionsContext)
   return (
     <div>
       <Header/>
@@ -41,11 +23,12 @@ export function Transactions(){
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
